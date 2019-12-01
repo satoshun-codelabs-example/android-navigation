6ec3831b9817ef98542fce06165d1c7e858ce91b
SatoShun <shun.sato1@gmail.com>
2019-12-01 09:25:01 +0000

    step one

--- app/src/main/java/com/example/android/codelabs/navigation/FlowStepFragment.kt
+++ app/src/main/java/com/example/android/codelabs/navigation/FlowStepFragment.kt
@@ -28,31 +28,31 @@ import androidx.navigation.Navigation
  */
 class FlowStepFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        setHasOptionsMenu(true)
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    setHasOptionsMenu(true)
 
-        val flowStepNumber = arguments?.getInt("flowStepNumber")
+    val flowStepNumber = arguments?.getInt("flowStepNumber")
 
-        // TODO STEP 8 - Use type-safe arguments - remove previous line!
+    // TODO STEP 8 - Use type-safe arguments - remove previous line!
 //        val safeArgs: FlowStepFragmentArgs by navArgs()
 //        val flowStepNumber = safeArgs.flowStepNumber
-        // TODO END STEP 8
+    // TODO END STEP 8
 
-        return when (flowStepNumber) {
-            2 -> inflater.inflate(R.layout.flow_step_two_fragment, container, false)
-            else -> inflater.inflate(R.layout.flow_step_one_fragment, container, false)
-        }
+    return when (flowStepNumber) {
+      2 -> inflater.inflate(R.layout.flow_step_two_fragment, container, false)
+      else -> inflater.inflate(R.layout.flow_step_one_fragment, container, false)
     }
+  }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<View>(R.id.next_button).setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.next_action)
-        )
-    }
+    view.findViewById<View>(R.id.next_button).setOnClickListener(
+      Navigation.createNavigateOnClickListener(R.id.next_action)
+    )
+  }
 }
